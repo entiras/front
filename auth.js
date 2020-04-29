083eec6f2af395e7b6a6927f87842283b6fac45e
operezcham <operezcham@gmail.com>
2020-04-29 18:09:13 -0500

    auth

--- auth.js
+++ auth.js
@@ -7,4 +7,22 @@ loader.hide = function () {
 }
 $(document).ready(function () {
     loader.show();
+    if (login.check()) {
+        content.logged();
+    } else {
+        content.guest();
+    }
 });
+var login = {};
+login.check = function () {
+    return false;
+}
+var content = {};
+content.logged = function () {
+    loader.hide();
+    $('.logged').removeClass('d-none');
+}
+content.guest = function () {
+    loader.hide();
+    $('.guest').removeClass('d-none');
+}
